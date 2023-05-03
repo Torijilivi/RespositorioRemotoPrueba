7a78b352a3ea5e51e062606be440e22d5866a83c
mtori <mtori@tori>
2023-05-03 09:14:26 +0200

    Escribir mensaje

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -3,8 +3,7 @@ package Paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Escribe este mensaje");
 	}
 
 }
